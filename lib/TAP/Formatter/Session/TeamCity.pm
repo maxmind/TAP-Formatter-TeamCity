e039682740dcc1277f20a9365482e78927b13ca0
Dave Rolsky <autarch@urth.org>
2015-12-08 17:12:51 -0600

    Bump version after release

--- lib/TAP/Formatter/Session/TeamCity.pm
+++ lib/TAP/Formatter/Session/TeamCity.pm
@@ -3,7 +3,7 @@ package TAP::Formatter::Session::TeamCity;
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use TAP::Parser::Result::Test;
 
--- lib/TAP/Formatter/TeamCity.pm
+++ lib/TAP/Formatter/TeamCity.pm
@@ -5,7 +5,7 @@ use 5.010;
 use strict;
 use warnings;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use TAP::Formatter::Session::TeamCity;
 
