e387599036ce3b1191eaa39fb9c85074859fd2a5
Dave Rolsky <autarch@urth.org>
2015-12-08 15:03:58 -0600

    Make $VERSION '0.05', not '0.050'

--- lib/TAP/Formatter/Session/TeamCity.pm
+++ lib/TAP/Formatter/Session/TeamCity.pm
@@ -3,7 +3,7 @@ package TAP::Formatter::Session::TeamCity;
 use strict;
 use warnings;
 
-our $VERSION = '0.050';
+our $VERSION = '0.05';
 
 use TAP::Parser::Result::Test;
 
--- lib/TAP/Formatter/TeamCity.pm
+++ lib/TAP/Formatter/TeamCity.pm
@@ -5,7 +5,7 @@ use 5.010;
 use strict;
 use warnings;
 
-our $VERSION = '0.050';
+our $VERSION = '0.05';
 
 use TAP::Formatter::Session::TeamCity;
 
